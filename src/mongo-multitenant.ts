a31e655d4060047cae6964ff727059d9188becac
henriquesml <henrique_schmeller@hotmail.com>
2022-03-22 21:33:59 -0300

    creating a constructor pattern in the main class

--- src/mongo-multitenant.ts
+++ src/mongo-multitenant.ts
@@ -8,6 +8,7 @@ const DEFAULT_CONFIGS = {
 export default class MongoMultitenant {
   private settings: SettingsProps
   private connection: ConnectionProps
+  private tenantDb: ConnectionProps
 
   private constructor(settings: SettingsProps) { 
     this.settings = settings
@@ -21,13 +22,20 @@ export default class MongoMultitenant {
     }) 
   }
 
-  getModel<T>({ tenantId, modelName }: GetModelProps) {
+  tenant(tenantId: string) {
     const dbName = `${this.settings.prefixDatabaseName}_${tenantId}`
-    if (this.connection.readyState === 1) {
-      const db = this.connection.useDb(dbName, { useCache: true })
-      this.settings.models.map(modelConfig => db.model(modelConfig.name, modelConfig.schema))
-      return db.model<T>(modelName)
+    if (this.connection.readyState !== 0) {
+      const tenantDb = this.connection.useDb(dbName, { useCache: true })
+      console.info(`DB switched to ${dbName}`)
+      this.settings.models.map(modelConfig => tenantDb.model(modelConfig.name, modelConfig.schema))
+      this.tenantDb = tenantDb
+      return this
     }
     throw new Error('Mongoose connection error.')
+  }
+
+  model<T>(name: string) {
+    const tenantDb = this.tenantDb
+    return tenantDb.model<T>(name)
   }
 }
